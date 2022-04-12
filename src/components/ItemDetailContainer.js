23053145971e85ae60e29e0412b50868bdf5367d
Agustin_Paleari <aguspaleari@gmail.com>
2022-04-12 11:18:37 -0300

    cambio products.js x datos en itemDetailContainer queda en cargando

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 
 import customFetch from "../utils/customFetch";
 import ItemDetail from "./ItemDetail";
-const {products} = require('../utils/products');
+
 
 const ItemDetailContainer = () => {
     const [dato, setDato] = useState({});
         
     useEffect(() => {
-        customFetch(2000, products[1])
+        customFetch(2000, dato[1])
             .then(result => setDato(result))
             .catch(err => console.log(err))
     }, []);
